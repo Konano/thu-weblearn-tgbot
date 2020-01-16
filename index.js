ec9a31a83e13f1824cd29f7398fdd15594962a24
Konano <nanoapezlk@gmail.com>
2020-01-16 23:34:03 +0800

    Add proxy

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const Telegraf = require('telegraf')
 var config = require('./config');
 
-const bot = new Telegraf(config.token)
+const SocksAgent = require('socks5-https-client/lib/Agent');
+const socksAgent = new SocksAgent({
+    socksHost: config.proxy.host,
+    socksPort: config.proxy.port,
+});
+
+const bot = new Telegraf(config.token, { telegram: { agent: socksAgent } })
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
