06fc9c672d76368e2bef044a7c26b76914db994b
Konano <nanoapezlk@gmail.com>
2020-01-16 22:48:43 +0800

    Init commit

---
+++ config.sample.js
@@ -0,0 +1,7 @@
+var config = {
+    debug: true,
+    name: 'THULearnBot',
+    token: '000000000:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,9 @@
+const Telegraf = require('telegraf')
+var config = require('./config');
+
+const bot = new Telegraf(config.token)
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
