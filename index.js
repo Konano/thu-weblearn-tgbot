59982a850fae87b232dac0ec008e568a88dcc4a4
Konano <nanoapezlk@gmail.com>
2021-09-16 22:53:41 +0800

    upgrade dependencies

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// const Telegraf = require('telegraf');
-const fs = require('fs');
-const htmlToText = require('html-to-text');
-const SocksAgent = require('socks5-https-client/lib/Agent');
-const thuLearnLib = require('thu-learn-lib');
-const moment = require('moment');
-const Trello = require('trello');
-const http = require('http');
-const https = require('https');
-const querystring = require('querystring');
+import { readFileSync, writeFileSync } from 'fs';
+import { htmlToText } from 'html-to-text';
+import { Learn2018Helper } from 'thu-learn-lib';
+import moment from 'moment';
+import { get } from 'http';
+import { request } from 'https';
+import { stringify } from 'querystring';
+import pkg from 'log4js';
+const { configure, getLogger } = pkg;
+import { config } from './config.js';
 
-const log4js = require('log4js');
-log4js.configure({
+configure({
     appenders: {
         console: { type: 'console' },
         logfile: { type: 'file', filename: 'log/runtime.log', maxLogSize: 1024 * 128, backups: 10 },
@@ -21,9 +20,7 @@ log4js.configure({
         default: { appenders: ['consoleFilter', 'fileFilter'], level: 'debug' }
     }
 });
-const logger = log4js.getLogger('default');
-
-var config = require('./config');
+const logger = getLogger('default');
 
 function sendMessage(msg, errmsg) {
     logger.debug('sendMessage Start');
@@ -34,12 +31,12 @@ function sendMessage(msg, errmsg) {
             parse_mode: 'Markdown'
         };
         const options = {
-            hostname: config.apiserver,
+            hostname: config.apiproxy,
             port: 443,
-            path: `/bot${config.token}/sendMessage?${querystring.stringify(params)}`,
+            path: `/bot${config.token}/sendMessage?${stringify(params)}`,
             method: 'GET'
         };
-        https.request(options, res => {
+        request(options, res => {
             logger.debug(`statusCode: ${res.statusCode}`);
             res.on('data', d => { logger.debug(d.toString()) })
         }).on('error', error => {
@@ -56,25 +53,7 @@ function sendMessage(msg, errmsg) {
     logger.debug('sendMessage Return');
 }
 
-/*
-let bot;
-if (config.proxy.status) {
-    const socksAgent = new SocksAgent({
-        socksHost: config.proxy.host,
-        socksPort: config.proxy.port,
-    });
-    bot = new Telegraf(config.token, { telegram: { agent: socksAgent } })
-} else {
-    bot = new Telegraf(config.token)
-}
-
-bot.catch((err, ctx) => {
-    logger.error(`Ooops, encountered an error for ${ctx.updateType}`, err)
-})
-bot.launch()
-*/
-
-const helper = new thuLearnLib.Learn2018Helper({ provider: () => { return { username: config.user.name, password: config.user.pwd }; } });
+const helper = new Learn2018Helper({ provider: () => { return { username: config.user.name, password: config.user.pwd }; } });
 
 async function delay(ms) {
     return await new Promise(resolve => setTimeout(resolve, ms));
@@ -87,7 +66,7 @@ function findCourse(predata, courseID) {
 }
 
 function reBlank(str) {
-    if (typeof str == "string") {
+    if (typeof str == 'string') {
         return str.replace(/\n\s*\n/gi, '\n').replace(/^\s*/m, '').replace(/\s*$/m, '');
     } else {
         return str;
@@ -95,7 +74,7 @@ function reBlank(str) {
 }
 
 function reMarkdown(str) {
-    if (typeof str == "string") {
+    if (typeof str == 'string') {
         return str.replace(/([\*\_\`\[])/gi, '\\$1');
     } else {
         return str;
@@ -201,7 +180,7 @@ function compareNotifications(courseName, nowdata, predata) {
                     `「${reMarkdown(courseName)}」发布了新的公告：` +
                     `[${reMarkdown(notification.title)}](${notification.url.replace(/learn2018/, 'learn')})\n` +
                     `====================\n` +
-                    reMarkdown(reBlank(htmlToText.fromString(notification.content))),
+                    reMarkdown(reBlank(htmlToText(notification.content))),
                     'New nofitication: sendMessage FAIL');
             }
         });
@@ -210,70 +189,7 @@ function compareNotifications(courseName, nowdata, predata) {
     }
 }
 
-const trello = new Trello(config.trello.key, config.trello.token);
-let TrelloLists = [];
-
-function TrelloGetCourseList(courseName) {
-    return TrelloLists.filter(list => list.name == courseName);
-}
-
-async function TrelloGetHomeworkCards(listID) {
-    try {
-        let cards = [];
-        await trello.getCardsOnList(listID).then(cardsList => {
-            cardsList.filter(card => card.labels.some(label => label.name == 'Homework')).forEach(
-                card => cards.push(card)
-            )
-        })
-        return cards;
-    } catch (err) {
-        logger.error('TrelloGetHomeworkCards Error');
-    }
-}
-
-const TRELLOERROR = Symbol("Trello Error");
-
-async function TrelloHomeworks(courseName, homeworks) {
-    try {
-        let list = TrelloGetCourseList(courseName);
-        if (list.length == 0) {
-            logger.error(`Trello List not found: ${courseName}`);
-            return;
-        }
-        let cards = await TrelloGetHomeworkCards(list[0].id);
-        if (cards == undefined) return;
-        // console.log(courseName, cards);
-        homeworks.forEach(homework => {
-            let card = cards.filter(card => card.name == homework.title);
-            if (card.length == 0) {
-                if (homework.submitted == false && homework.deadline > (new Date())) {
-                    logger.info(`Trello: addCard "${homework.title}"`)
-                    trello.addCard(homework.title, '', list[0].id)
-                        .then(newcard => {
-                            trello.addLabelToCard(newcard.id, config.trello.label);
-                            trello.addDueDateToCard(newcard.id, homework.deadline);
-                        });
-                }
-            } else {
-                card = card[0];
-                if (card.due != homework.deadline.toISOString()) {
-                    logger.info(`Trello: Update due "${homework.title}"`)
-                    trello.updateCard(card.id, 'due', homework.deadline);
-                }
-                if (homework.submitted) {
-                    logger.info(`Trello: Update dueComplete "${homework.title}"`)
-                    trello.updateCard(card.id, 'dueComplete', true);
-                    trello.updateCard(card.id, 'closed', true);
-                }
-            }
-        });
-    } catch (err) {
-        logger.error(err);
-        throw TRELLOERROR;
-    }
-}
-
-const TIMEOUT = Symbol("Timeout");
+const TIMEOUT = Symbol('Timeout');
 
 async function getCourseList(semester) {
     return Promise.race([
@@ -283,9 +199,6 @@ async function getCourseList(semester) {
 }
 
 (async () => {
-    await trello.getListsOnBoardByFilter(config.trello.board, 'open').then(
-        lists => lists.forEach(list => TrelloLists.push(list))
-    )
 
     while (true) {
         try {
@@ -302,13 +215,13 @@ async function getCourseList(semester) {
     let predata = [];
 
     try {
-        predata = await JSON.parse(fs.readFileSync('data.json', 'utf8'));
+        predata = await JSON.parse(readFileSync('data.json', 'utf8'));
     } catch (err) {
         logger.error(err);
         let tasks = [];
         let courses = [];
         for (let semester of config.semesters) {
-            courses = courses.concat(await helper.getCourseList(semester));
+            courses = courses.concat(await getCourseList(semester));
         }
         for (let course of courses) {
             tasks.push((async () => {
@@ -326,7 +239,7 @@ async function getCourseList(semester) {
         };
 
         await Promise.all(tasks);
-        fs.writeFileSync('data.json', JSON.stringify(predata, null, 4));
+        writeFileSync('data.json', JSON.stringify(predata, null, 4));
     };
 
     while (true) {
@@ -339,7 +252,7 @@ async function getCourseList(semester) {
             // logger.debug('Getting course list...');
             let courses = [];
             for (let semester of config.semesters) {
-                courses = courses.concat(await helper.getCourseList(semester));
+                courses = courses.concat(await getCourseList(semester));
             }
             // logger.debug('Got course list.');
             // logger.debug(courses);
@@ -352,10 +265,6 @@ async function getCourseList(semester) {
                     // course.questions = await helper.getAnsweredQuestionList(course.id);
 
                     logger.debug(`Course <${course.name}>: files ${course.files.length} notifications ${course.notifications.length} homeworks ${course.homeworks.length}`);
-
-                    // logger.debug('TrelloHomeworks');
-                    await TrelloHomeworks(course.name, course.homeworks);
-                    // logger.debug('TrelloHomeworks end');
 
                     await new Promise((resolve => {
                         nowdata.push(course);
@@ -384,11 +293,11 @@ async function getCourseList(semester) {
                 }
             }
 
-            fs.writeFileSync('data.json', JSON.stringify(nowdata, null, 4));
+            writeFileSync('data.json', JSON.stringify(nowdata, null, 4));
             predata = nowdata;
             preTimestamp = nowTimestamp;
             logger.debug('Checked.');
-            http.get(config.heartbeat).on('error', error => {
+            get(config.heartbeat).on('error', error => {
                 logger.debug('alert error');
                 logger.debug(error);
             }).end(() => {
@@ -398,7 +307,7 @@ async function getCourseList(semester) {
             if (err === TIMEOUT) {
                 logger.error('Timeout.');
                 continue;
-            } else if (err !== TRELLOERROR) {
+            } else {
                 logger.error(err);
                 while (true) {
                     try {
@@ -420,84 +329,6 @@ async function getCourseList(semester) {
     }
 })();
 
-/*
-function rankCard(card) {
-    if (card.due == null) return 0;
-    if (card.dueComplete) return 1;
-    if (card.due <= (new Date()).toISOString()) return 2;
-    return 3;
-}
-
-async function sortList(listID) {
-    let due = null;
-    await trello.getCardsOnList(listID).then(cardsList => {
-        let _pos = cardsList.map(x => x.pos).sort((a, b) => a - b);
-        // console.log(_pos)
-        cardsList.sort((a, b) => {
-            if (rankCard(a) != rankCard(b)) {
-                return rankCard(a) - rankCard(b)
-            } else if (rankCard(a) == 3) {
-                return (a.due < b.due ? 1 : a.due > b.due ? -1 : a.pos - b.pos) 
-            } else {
-                return a.pos - b.pos
-            }
-        });
-        for (let i = 0; i < cardsList.length; i++) if (cardsList[i].pos != _pos[i]) {
-            // console.log(cardsList[i].pos, _pos[i])
-            trello.updateCard(cardsList[i].id, 'pos', _pos[i]);
-        }
-        try {
-            due = cardsList[cardsList.length - 1].due;
-        } catch (_) {
-            due = null;
-        }
-    });
-    return due;
-}
-
-(async () => {
-    while (true) {
-        try {
-            logger.debug('Start Trello sorting...');
-            let TrelloLists = [];
-            while (true) {
-                try {
-                    await Promise.race([
-                        trello.getListsOnBoardByFilter(config.trello.board, 'open').then(
-                            lists => lists.forEach(list => TrelloLists.push(list))
-                        ),
-                        new Promise((resolve, reject) => setTimeout(() => reject(TIMEOUT), 60 * 1000))
-                    ]);
-                    break;
-                } catch (_) { logger.error('getListsOnBoard timeout.'); }
-                await delay(60 * 1000);
-            }
-            TrelloLists = await Promise.all(TrelloLists.map(async list => {
-                list.due = await sortList(list.id);
-                return list;
-            }));
-            let _pos = TrelloLists.map(x => x.pos).sort((a, b) => a - b);
-            TrelloLists.sort((a, b) => {
-                if (a.due == null && b.due == null) {
-                    return a.pos - b.pos
-                } else if (a.due == null || b.due == null) {
-                    return (a.due == null ? 1 : -1);
-                } else
-                return (a.due > b.due ? 1 : a.due < b.due ? -1 : a.pos - b.pos)
-            });
-            for (let i = 0; i < TrelloLists.length; i++) if (TrelloLists[i].pos != _pos[i]) {
-                trello.makeRequest('put', `/1/lists/${TrelloLists[i].id}/pos`, { value: _pos[i] });
-            }
-            logger.debug('Stop Trello sorting');
-        } catch (err) {
-            logger.error(err);
-            logger.error('Error in Trello sorting');
-        }
-        await delay(60 * 1000);
-    }
-})();
-*/
-
 (async () => {
     while (true) {
         try {
@@ -506,7 +337,7 @@ async function sortList(listID) {
                 logger.info('global.gc()');
             }
         } catch (e) {
-            console.log("`nodejs --expose-gc index.js`");
+            console.log('`node --expose-gc index.js`');
         }
         await delay(3600 * 1000);
     }
