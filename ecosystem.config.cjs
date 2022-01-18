b4037bde4354779e51ba315a82c831a36803d095
Konano <nanoapezlk@gmail.com>
2022-01-19 02:29:58 +0800

    chore: add pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: 'Web Learner',
+        cmd: 'index.js',
+        // interpreter: 'python3',
+        // autorestart: false,
+        // watch: true,
+    }]
+};
